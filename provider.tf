39949ce98eff8a9d232171c991d0a84a234362fd
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-06-14 13:42:51 +0530

    Initial blue print.

---
+++ provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us-east"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us-west"
+  region = "us-west-2"
+}
