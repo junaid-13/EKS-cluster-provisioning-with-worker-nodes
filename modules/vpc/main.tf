0016f0aa2cee168b5eaa351d636321e89e5f455b
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-06-14 14:01:11 +0530

    Added the private subnet for the dev-vpc.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -25,3 +25,10 @@ resource "aws_subnet" "dev-eks-public-subnet" {
   cidr_block = var.dev-eks-vpc-public-subnet
   availability_zone = var.dev-eks-public-subnet-az
 }
+
+resource "aws_subnet" "dev-eks-private-subnet" {
+  vpc_id = aws_vpc.dev-eks-vpc.id
+  cidr_block = var.dev-vpc-private-subnet-cidr
+  availability_zone = var.dev-vpc-private-subnet-az
+  
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -14,3 +14,29 @@ variable "Environment" {
   type        = array(string)
     default     = ["Development", "Production"] 
 }
+
+variable "dev-eks-vpc-public-subnet" {
+  description = "Public subnet CIDR block for the development VPC"
+  type        = string
+  default     = "10.0.1.0/24"
+  
+}
+
+variable "dev-eks-public-subnet-az" {
+  description = "Availability Zone for the development VPC public subnet"
+  type        = string
+  default     = "us-west-1a"
+}
+
+variable "dev-vpc-private-subnet-cidr" {
+  description = "Private subnet CIDR block for the development VPC"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "dev-vpc-private-subnet-az" {
+  description = "Availability Zone for the development VPC private subnet"
+  type        = string
+  default     = "us-west-1b"
+  
+}
